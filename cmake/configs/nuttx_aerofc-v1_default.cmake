PX4/PX4-Autopilot

cmake: add serial param & ROMFS generation to the build system
- only a few configs done yet
- probably will make setting board_serial_ports optional

cmake/configs/nuttx_aerofc-v1_default.cmake
@@ -1,6 +1,12 @@
 
 px4_nuttx_configure(HWCLASS m4 CONFIG nsh ROMFS y ROMFSROOT px4fmu_common)
 
+# user-configurable UART ports
+set(board_serial_ports
+	GPS1:/dev/ttyS5
+	TEL1:/dev/ttyS3
+	TEL2:/dev/ttyS2)
+
 set(config_uavcan_num_ifaces 2)
 
 set(config_module_list

cmake/configs/nuttx_omnibus-f4sd_default.cmake
@@ -1,6 +1,11 @@
 
 px4_nuttx_configure(HWCLASS m4 CONFIG nsh ROMFS y ROMFSROOT px4fmu_common)
 
+# user-configurable UART ports
+set(board_serial_ports
+	URT3:/dev/ttyS1
+	URT6:/dev/ttyS2)
+
 set(config_uavcan_num_ifaces 1)
 
 set(config_module_list

EvaKristiansen/Firmware

cmake/configs/nuttx_aerofc-v1_default.cmake
@@ -1,6 +1,12 @@
 
 px4_nuttx_configure(HWCLASS m4 CONFIG nsh ROMFS y ROMFSROOT px4fmu_common)
 
+# user-configurable UART ports
+set(board_serial_ports
+	GPS1:/dev/ttyS5
+	TEL1:/dev/ttyS3
+	TEL2:/dev/ttyS1)
+
 set(config_module_list
 	#
 	# Board support modules

cmake/configs/nuttx_omnibus-f4sd_default.cmake
@@ -1,6 +1,11 @@
 
 px4_nuttx_configure(HWCLASS m4 CONFIG nsh ROMFS y ROMFSROOT px4fmu_common)
 
+# user-configurable UART ports
+set(board_serial_ports
+	TEL2:/dev/ttyS1
+	URT6:/dev/ttyS2)
+
 set(config_module_list
 	#
 	# Board support modules